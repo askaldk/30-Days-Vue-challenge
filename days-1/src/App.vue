db21882887b858b6f3e88a7a1fffe2f995d82a94
aska <aska0522@gmail.com>
2019-07-09 21:12:57 +0800

    print "Hello World!"

---
+++ days-1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>{{key}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return{
+    key:"Hello World!"
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
