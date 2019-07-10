a053b9c0fdacbc196d6f18f742094281e6a81ff9
aska <aska0522@gmail.com>
2019-07-11 00:31:53 +0800

    v-on button click

---
+++ days-2/simple-binding-example/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1>{{fish}}</h1>
+    <p>{{introduce}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      fish:"Hello World!",
+      introduce:"By Ang Qian who lives in Taiwan"
+
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+html,body{
+  background-color:darkcyan;
+  height: 100%;
+}
+#app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+}
+h1 {
+  margin-top: 0;
+}
+p{
+  margin-top: 0;
+  font-size:18px;
+}
+</style>
---
+++ days-2/simple-data-change-example/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <p>By{{author}}from{{country}}</p>
+    <button  v-on:click="changeButton">
+      changeButton
+    </button>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return{
+      title:"Hello World",
+      author:" Ang Qian ",
+      country:" Taiwan"
+    }
+  },
+  methods:{
+    changeButton(){
+      this.title = this.title === "Hello World" ?
+       "What's wrong":
+       "Hello World";
+    }
+  }
+}
+</script>
+
+<style>
+html,body{
+  background-color: aquamarine;
+
+}
+#app {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+}
+h1{
+  margin-top: 0;
+
+}
+p{
+  margin-top: 0;
+  font-size: 18px;
+}
+</style>
