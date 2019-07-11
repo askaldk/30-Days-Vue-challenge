04c3085a57aa54eca4b7bcc8000826a53f8034e5
aska <aska0522@gmail.com>
2019-07-12 00:33:26 +0800

    another way of v-on:click

---
+++ days-3/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <p>By {{author}} from {{country}}</p>
+    <button v-on:click="changeButton">{{ buttonTag  }}</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return{
+      title:"Hello World!",
+      author:"Ang Qian",
+      country:"Taiwan",
+      buttonTag: "Hello World!"
+    }
+  },
+
+  methods:{
+  changeButton(){
+    if(this.buttonTag === 'Hello World!'){
+      this.buttonTag="What's up!";
+      }else{
+      this.buttonTag= "Hello World!";
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+}
+h1{
+  margin-top: 0;
+}
+p{
+  margin-top:0;
+}
+</style>
