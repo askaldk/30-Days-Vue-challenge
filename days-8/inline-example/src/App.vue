7351496e339ec441fa67f82eaf129d26d284ada0
aska <aska0522@gmail.com>
2019-07-15 15:30:08 +0800

    Another way of days-7

---
+++ days-8/inline-example/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="card">
+      <header class="card-header card-header-title">
+        <span>Original:</span>
+        {{message}}
+      </header>
+
+      <header class="card-header card-header-title">
+        <span>Reversed:</span>
+        {{message.split('').reverse().join('')}}
+      </header>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      message:'Greetings!'
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  max-width: 300px;
+  margin-top:20px;
+  margin-left: 10px;
+}
+header span{
+  font-weight: 400;
+}
+</style>
---
+++ days-8/methods-example/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="card">
+      <header class="card-header card-header-title">
+        <span>Original:</span>
+        {{message}}        
+      </header>
+
+      <header class="card-header card-header-title">
+        <span>Reverse:</span>
+        {{reverseMessage(message)}}
+
+      </header>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      message:'Greetings!'
+    }
+  },
+  methods:{
+    reverseMessage(String){
+      return String.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  max-width: 300px;
+  margin-top:20px;
+  margin-left: 10px; 
+}
+header span{
+  font-weight: 400;
+}
+</style>
