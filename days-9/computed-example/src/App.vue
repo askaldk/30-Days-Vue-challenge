daa6b1e7189672fb54de7df9c2740e11d945facc
aska <aska0522@gmail.com>
2019-07-15 18:59:34 +0800

    watch and comptued

---
+++ days-9/computed-example/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div class="field">
+      <label class="label">KM</label>
+      <input v-model="km" class="input" type="number" placeholder="Tyep a length in KM!">
+    </div>
+    
+    <div class="field">
+      <label class="label">M</label>
+      <input v-model="m" class="input" type="number" placeholder="Type a length in M!">
+    </div>
+  
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      km:0
+    }
+  },
+  computed:{
+    m:{
+      get(){
+        return this.km*1000
+      },
+      set(newValue){
+        this.km = newValue/1000
+      }
+    }
+  }
+ 
+}
+</script>
+
+<style>
+html,body{
+  background-color:aliceblue;
+}
+#app {
+  
+ height: 100vh;
+ display: flex;
+ flex-direction: column;
+ max-width: 300px;
+ margin-top:20px;
+ margin-left: 10px;
+}
+
+</style>
---
+++ days-9/watchers-example/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="field">
+      <label class="label">KM</label>
+      <input type="number" v-model="km" class="input" placeholder="input some KM value">
+    </div>
+
+    <div class="field">
+      <label class="label">M</label>
+      <input type="number" v-model="m" class="input" placeholder="input some KM value">
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      km:0,
+      m:0
+    }
+  },
+  watch:{
+    km(){
+      this.m=this.km ? this.km*1000 : 0
+    },
+    m(){
+      this.km=this.m ? this.m/1000 : 0
+    }
+  }
+
+}
+</script>
+
+<style>
+html,body{
+  background-color: blanchedalmond;
+}
+
+#app {
+  max-width: 300px;
+  margin-top:20px;
+  margin-left: 10px;
+
+}
+</style>
