a90e67b694a5bae6a09216f07cd0011759faae44
aska <aska0522@gmail.com>
2019-07-16 23:45:16 +0800

    call api

---
+++ days-10/created-example/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          {{name}}
+        </p>
+      </header>
+    </div>
+
+    <div class="card-content">
+      <div class="content">
+        <p>{{company.name}}</p>
+        <p>{{company.catchPhrase}}</p>
+      </div>
+    </div>
+
+    <div class="card-footer">
+      <a href="#" class="card-footer-item">{{email}}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios  from 'axios'
+export default {
+  name: 'app',
+  data(){
+    return{
+      name:'Loading...',
+      email:'Loading...',
+      company:{
+        name:'Loading...',
+        catchPhrase:'Loading...'
+      }
+    }
+  },
+  created(){
+    axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    ).then((response)=>{
+      const data = response.data
+      const randomUser =response.data[
+        Math.floor(Math.random()*data.length)
+      ]
+
+      this.name = randomUser.name
+      this.email = randomUser.email
+      this.company.name = randomUser.company.name
+      this.company.catchPhrase =randomUser.company.catchPhrase
+    })
+  }
+}
+</script>
+
+<style>
+html,body{
+  background-color: beige;
+}
+#app {
+  margin-top: 20px;
+  margin-left: 10px;
+  max-width: 300px;
+}
+</style>
---
+++ days-10/mounted-example/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          {{name}}
+        </p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+         <p>{{company.name}}</p> 
+         <p>{{company.catchPhrase}}</p>
+        </div>
+
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">{{email}}</a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data(){
+    return{
+      name:'Loading...',
+      email:'Loading...',
+      company:{
+        name:'Loading...',
+        catchPhrase:'Loading...'
+      }
+    }
+  },
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>{
+      const data = response.data
+      const randomUser = data[Math.floor(Math.random()*data.length)]
+
+      this.name = randomUser.name
+      this.email = randomUser.email
+      this.company.name = randomUser.company.name
+      this.company.catchPhrase=randomUser.company.catchPhrase
+    })
+  }
+  
+  
+}
+</script>
+
+<style>
+#app {
+  margin-top:20px;
+  margin-left: 10px;
+  max-width: 300px;
+  
+}
+
+</style>
