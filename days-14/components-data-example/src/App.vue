fbc9987aceb35f59557bfb98fe7919171c40dfe9
aska <aska0522@gmail.com>
2019-07-23 21:46:46 +0800

    component-example

---
+++ days-14/components-data-example/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <p>Three different components.</p>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import counter from './components/couter1.vue'
+export default {
+  name: 'app',
+  components:{
+    counter
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  padding-top: 10px;
+  background: #e6ecf1;
+}
+
+#app {
+  height: 100%;
+  max-width: 1000px;
+  padding: 12px;
+}
+
+p {
+  padding-bottom: 5px;
+}
+
+.component-button {
+  margin-left: 10px;
+  display: inline-block;
+  margin-top: 10px;
+  background-color: #00d1b2;
+  color: #FFF;
+  padding: 5px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+</style>
---
+++ days-14/components-data-example/src/components/couter1.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+     <button class="component-button" @click="counter.count++" v-for="counter in counters" :key="counter.id">
+      You clicked me {{counter.count}} times
+      </button>
+</div>
+</template>
+<script>
+export default {
+    data()
+    {
+        return{
+           counters:[
+               {
+                   id:1,
+                   count:0
+               },
+               {
+                   id:2,
+                   count:0    
+               },
+               {
+                   id:3,
+                   count:0
+               }
+           ]
+        }
+    },
+
+    
+}
+</script>
+
