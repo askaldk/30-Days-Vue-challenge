dec7fd936a37825945fc403a323f813bca8e365c
aska <aska0522@gmail.com>
2019-07-26 13:52:20 +0800

    Array control

---
+++ days-17/custom-events-example/src/components/ParentComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ChildComponent :numbers="numbers" @NumberAdded="numbers.push($event)"></ChildComponent>
+  </div>
+</template>
+<script>
+import ChildComponent from "./ChildComponent";
+export default {
+  data() {
+    return {
+      numbers: [1, 2, 3]
+    };
+  },
+  components: {
+    ChildComponent
+  } 
+};
+</script>
+
