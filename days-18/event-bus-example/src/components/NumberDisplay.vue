cbc9da275d0ff572cb1eb56533099de4a4c5c3b5
aska <aska0522@gmail.com>
2019-07-28 15:48:00 +0800

    EventBus

---
+++ days-18/event-bus-example/src/components/NumberDisplay.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{numbers}}</h1>
+  </div>
+</template>
+<script>
+import { EventBus } from "../EventBus.js";
+export default {
+  name: "NumberDisplay",
+  data() {
+    return {
+      numbers: [1, 2, 3]
+    };
+  },
+  created() {
+    EventBus.$on("NumberAdded", number => {
+      this.numbers.push(number);
+    });
+  }
+};
+</script>
---
+++ days-18/event-bus-example/src/components/NumberSubmit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input type="number" v-model="number" />
+    <button @click="addNumber">Add new number</button>
+  </div>
+</template>
+<script>
+import { EventBus } from "../EventBus.js";
+export default {
+  name: "NumberSubmit",
+  data() {
+    return {
+      number: 0
+    };
+  },
+  methods: {
+    addNumber() {
+      EventBus.$emit("NumberAdded", Number(this.number));
+    }
+  }
+};
+</script>
