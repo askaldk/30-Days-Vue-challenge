d6751aba926a33b5d244bb4324fdbf510bbb8e65
aska <aska0522@gmail.com>
2019-07-31 20:41:23 +0800

    global-store

---
+++ days-20/simple-global-store-example/src/components/NumberDisplay.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>{{storeState.numbers}}</h2>
+</template>
+<script>
+import { store } from "../store";
+export default {
+  name: "NumberDisplay",
+  data() {
+    return {
+      storeState: store.state
+    };
+  }
+};
+</script>
